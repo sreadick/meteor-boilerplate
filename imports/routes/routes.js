412f8f0f4945f11064f3de33dd611421619e7031
Sky Readick <szreadick@gmail.com>
2017-07-17 20:32:58 -0400

    Upgrade packages to current versions

--- imports/routes/routes.js
+++ imports/routes/routes.js
@@ -1,40 +1,41 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
+// import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 
 import Signup from '../ui/Signup';
 import Login from '../ui/Login';
 import Dashboard from '../ui/Dashboard';
 import NotFound from '../ui/NotFound';
 
+const history = createBrowserHistory();
+
 const unauthenticatedPages = ['/', '/signup'];
 const authenticatedPages = ['/dashboard'];
-const onEnterPublicPage = () => {
-  if (Meteor.userId()) {
-    browserHistory.replace('/dashboard');
-  }
-};
-const onEnterPrivatePage = () => {
-  if (!Meteor.userId()) {
-    browserHistory.replace('/');
-  }
-};
+
 export const onAuthChange = (isAuthenticated) => {
-  const pathname = browserHistory.getCurrentLocation().pathname;
+  const pathname = history.location.pathname;
   const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
   const isAuthenticatedPage = authenticatedPages.includes(pathname);
 
   if (isUnauthenticatedPage && isAuthenticated) {
-    browserHistory.replace('/dashboard');
+    history.replace('/dashboard');
   } else if (isAuthenticatedPage && !isAuthenticated) {
-    browserHistory.replace('/');
+    history.replace('/');
   }
 };
 export const routes = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Login} onEnter={onEnterPublicPage}/>
-    <Route path="/signup" component={Signup} onEnter={onEnterPublicPage}/>
-    <Route path="/dashboard" component={Dashboard} onEnter={onEnterPrivatePage}/>
-    <Route path="*" component={NotFound}/>
+  <Router history={history}>
+    <Switch>
+      <Route exact path="/" render={() => {
+        return Meteor.userId() ? <Redirect to="/dashboard" /> : <Login/>
+      }} />
+      <Route path="/signup" render={() => {
+        return Meteor.userId() ? <Redirect to="/dashboard" /> : <Signup/>
+      }} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="*" component={NotFound}/>
+    </Switch>
   </Router>
 );
